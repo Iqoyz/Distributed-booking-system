05c4ae34581c262f1595fb23e93b7135452df16f
Iqoyz <Chanyonqi@hotmail.com>
2025-02-22 20:39:09 +0800

    chore: set up build configuration and automate build process

---
+++ scripts/build_and_run_windows.bat
@@ -0,0 +1,40 @@
+@echo off
+set SERVER_DIR=..\server
+set BUILD_DIR=%SERVER_DIR%\build
+set SERVER_EXE=booking_system_server.exe
+set TEST_EXE=server_test.exe
+
+:: Check if an argument was provided
+if "%1"=="" (
+    echo Usage: build_and_run.bat [server|test]
+    exit /b 1
+)
+
+echo Configuring project with CMake...
+cmake -S %SERVER_DIR% -B %BUILD_DIR% -G "MinGW Makefiles"
+
+echo Building project...
+cmake --build %BUILD_DIR%
+
+:: Run based on user input
+if /I "%1"=="server" (
+    echo Running server executable...
+    if exist %BUILD_DIR%\%SERVER_EXE% (
+        cd %BUILD_DIR%
+        .\%SERVER_EXE%
+    ) else (
+        echo Server executable not found!
+    )
+) else if /I "%1"=="test" (
+    echo Running test executable...
+    if exist %BUILD_DIR%\%TEST_EXE% (
+        cd %BUILD_DIR%
+        .\%TEST_EXE%
+    ) else (
+        echo Test executable not found!
+    )
+) else (
+    echo Invalid option! Use 'server' or 'test'.
+)
+
+cd %SERVER_DIR%
